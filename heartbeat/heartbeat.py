fd75ff4458aa963a20e4cdaa912e82abec9dcf45
Zsolt Fekete <zsolt@enbrite.ly>
2015-04-21 09:49:42 +0200

    new feature: uptime

--- heartbeat/heartbeat.py
+++ heartbeat/heartbeat.py
@@ -2,21 +2,29 @@ from BaseHTTPServer import HTTPServer
 from BaseHTTPServer import BaseHTTPRequestHandler
 import json
 import threading
+import datetime
 
 import git_sha
 
-class HeartbeatRequestHandler (BaseHTTPRequestHandler) :
+global starting_time
+starting_time = None
+
+class HeartbeatRequestHandler(BaseHTTPRequestHandler):
   def do_GET(self) :
-    if self.path == "/heartbeat" :
+    global starting_time
+    if self.path == '/heartbeat' :
       self.send_response(200)
-      self.send_header("Content-type:", "text/html")
-      self.wfile.write("\n")
-      obj = {"status": "running", "build": git_sha.git_sha}
+      self.send_header('Content-type:', 'text/html')
+      self.wfile.write('\n')
+      now = datetime.datetime.now()
+      uptime = str(now - starting_time)
+      obj = {'status': 'running', 'build': git_sha.git_sha, 'uptime': uptime}
       json.dump(obj, self.wfile)
       self.wfile.write("\n")
 
 def run_heartbeat_service(port, addr=''):
-  """Starts a HTTP server for prometheus metrics as a daemon thread."""
+  global starting_time
+  starting_time = datetime.datetime.now()
   class HeartbeatServer(threading.Thread):
     def run(self):
       httpd = HTTPServer((addr, port), HeartbeatRequestHandler)
