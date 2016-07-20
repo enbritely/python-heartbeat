d6d5e6d8d21873532ccd4562574335a24a8e9036
Adam Tauber <asciimoo@gmail.com>
2016-07-20 16:44:14 +0200

    call git rev-parse from the project dir

--- heartbeat/heartbeat.py
+++ heartbeat/heartbeat.py
@@ -3,6 +3,7 @@ import json
 import threading
 import datetime
 import subprocess
+import os
 
 if sys.version > '3':
     from http.server import HTTPServer
@@ -27,7 +28,11 @@ class HeartbeatRequestHandler(BaseHTTPRequestHandler):
                 import git_sha
                 commit_hash = git_sha.git_sha
             except ImportError:
-                commit_hash = subprocess.check_output(["git", "rev-parse", "HEAD"])
+                p = subprocess.Popen(["git", "rev-parse", "HEAD"],
+                                     stdout=subprocess.PIPE,
+                                     cwd=os.path.dirname(os.path.realpath(__file__)))
+                p.wait()
+                commit_hash = p.stdout.read().strip()
 
             obj = {'status': 'running', 'build': commit_hash.strip(), 'uptime': uptime}
             self.wfile.write(('\n' + json.dumps(obj) + "\n").encode('utf-8'))
